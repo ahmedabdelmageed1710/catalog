80bf43b0b7b9c8e049feef2f0c76916481b8a340
omarelsawy <omar.elsawy@yahoo.com>
2019-03-25 18:51:28 +0200

    'm'

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
+import psycopg2;
 
 Base = declarative_base()
 
